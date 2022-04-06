6d417d35c895bcc20f384acf8f4ddc1af963f0eb
constancahtfs <constancahtfs@gmail.com>
2022-04-07 00:20:52 +0100

    Fixed SQL bugs

--- src/Databases/MySQLScripts/CreateDatabase.sql
+++ src/Databases/MySQLScripts/CreateDatabase.sql
@@ -63,12 +63,12 @@ DELIMITER ;
 
 DELIMITER //
 
-CREATE DEFINER=`Software`@`localhost` PROCEDURE InsertMedicao(zona INT(11), sensor INT(11), date_time TIMESTAMP, measurement DECIMAL(5,2))
+CREATE DEFINER=`Software`@`localhost` PROCEDURE InsertMedicao(zona INT(11), sensor INT(11), tiposensor CHAR(1), date_time TIMESTAMP, measurement DECIMAL(5,2))
 BEGIN
 	SELECT *  FROM medicao;
-    insert into medicao(IDMedicao, IDSensor, IDZona, Valor, Datetime)
+    insert into medicao(IDMedicao, IDSensor, TipoSensor, IDZona, Valor, Datetime)
     values
-    (uuid(), sensor, zona, measurement, date_time);
+    (uuid(), sensor, tiposensor, zona, measurement, date_time);
 END //
 
 DELIMITER ;
