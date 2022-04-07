5c51e7b87954ccb575a43525bff2aaf06d6cf35f
FredSett19 <fnnao@iscte-iul.pt>
2022-04-07 18:08:25 +0100

    Acho que ainda não está completo, tentei da maneira de baixo que está comentada mas sem sucesso

--- src/Databases/MySQLScripts/SPEditarParametrosCultura.sql
+++ src/Databases/MySQLScripts/SPEditarParametrosCultura.sql
@@ -1,19 +1,75 @@
 DELIMITER $$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `EditarParametrosCultura` (IN `IDCultura` VARCHAR(50), IN `TipoSensor` VARCHAR(100), IN `ValorMax` DECIMAL(5,2), IN `ValorMin` DECIMAL(5,2), IN `ToleranciaMax` DECIMAL(5,2), IN `ToleranciaMin` DECIMAL(5,2))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `EditarParametroCultura` (IN `IDCultura` VARCHAR(50), IN `TipoSensor` CHAR(1), IN `ValorMax` DECIMAL(5,2), IN `ValorMin` DECIMAL(5,2), IN `ToleranciaMax` DECIMAL(5,2), IN `ToleranciaMin` DECIMAL(5,2) )  BEGIN
     SET
     `IDCultura` := CONCAT("'", `IDCultura`, "'"),
     `TipoSensor` := CONCAT("'", `TipoSensor`, "'"),
     `ValorMax` := CONCAT("'", `ValorMax`, "'"),
-    `ValorMin` := CONCAT("'", `ValorMin`, "'");
-    `ToleranciaMax` := CONCAT("'", `ToleranciaMax`, "'");
+    `ValorMin` := CONCAT("'", `ValorMin`, "'"),
+    `ToleranciaMax` := CONCAT("'", `ToleranciaMax`, "'"),
     `ToleranciaMin` := CONCAT("'", `ToleranciaMin`, "'");
 
+/*
     SET
-    `IDCultura` := REPLACE(`IDCultura`,'''',''),
-    `TipoSnesor` := REPLACE(`TipoSensor`,'''',''),
-    `ValorMax` := REPLACE(`ValorMax`,'''',''),
-    `ValorMin` := REPLACE(`ValorMin`,'''',''),
-    `TolearnciaMax` := REPLACE(`ToleranciaMax`,'''',''),
-    `ToleranciaMin` := SUBSTRING(`ToleranciaMin`, 1, 1);
+    `p_Nome` := REPLACE(`p_Nome`,'''',''),
+    `p_Email` := REPLACE(`p_Email`,'''',''),
+    `p_Role` := REPLACE(`p_Role`,'''',''),
+    `p_Role` := SUBSTRING(`p_Role`, 1, 1);
 
+*/
+
+    INSERT INTO parametrocultura(IDCultura, TipoSensor, ValorMax, ValorMin, ToleranciaMax, ToleranciaMin)
+    VALUES (`IDCultura`, `TipoSensor`, `ValorMax`, `ValorMin`, `ToleranciaMax`, `ToleranciaMin`);
+
+    FLUSH PRIVILEGES;
+END$$
+
+DELIMITER ;
+
+
+
+
+
+
+
+/* TENTEI E NÃO DEU BEM
+
+CREATE PROCEDURE Masterinsertupdatedelete (@IDCultura     VARCHAR(50),
+                                          @TipoSensor    CHAR(1),
+                                          @ValorMax      DECIMAL(5,2),
+                                          @ValorMin      DECIMAL(5, 2),
+                                          @ToleranciaMax DECIMAL(5, 2),
+                                          @ToleranciaMin DECIMAL(5, 2),
+                                          @StatementType NVARCHAR(20) = '')
+
+AS
+    BEGIN
+        IF @StatementType = 'INSERT'
+            BEGIN
+                INSERT INTO parametrocultura
+                            (IDCultura,
+                            TipoSensor,
+                            ValorMax,
+                            ValorMin,
+                            ToleranciaMax,
+                            ToleranciaMin)
+                VALUES      (@IDCultura,
+                            @TipoSensor,
+                            @ValorMax,
+                            @ValorMin,
+                            @ToleranciaMax,
+                            @ToleranciaMin)
+            END
+
+        IF @StatementType = 'UPDATE'
+                BEGIN
+                    UPDATE parametrocultura
+                    SET    ValorMax = @ValorMax,
+                           ValorMin = @ValorMin
+                           ToleranciaMax = @ToleranciaMax
+                           ToleranciaMin = @ToleranciaMin
+                    WHERE  IDCultura = @IDCultura,
+                           TipoSensor = @TipoSensor
+                END
+    END
+*/
