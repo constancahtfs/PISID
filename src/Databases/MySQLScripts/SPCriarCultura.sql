8ee70b0186df82fe86f6294864335b0e76b7bac4
moonzn <raul.0515@hotmail.com>
2022-04-09 19:20:35 +0100

    criar/apagar/atribuir cultura v1.0 refactor inserir mediçao

--- src/Databases/MySQLScripts/SPCriarCultura.sql
+++ src/Databases/MySQLScripts/SPCriarCultura.sql
@@ -1,39 +1,53 @@
 DELIMITER $$
 
+DROP PROCEDURE IF EXISTS `CriarCultura` $$
 CREATE DEFINER=`root`@`localhost`
-PROCEDURE `CriarCultura` (IN `p_NomeCultura` VARCHAR(150), IN `p_EmailInvestigador` VARCHAR(64), IN `p_Zona` INT(1))
+PROCEDURE `CriarCultura` (IN `nome_cultura` VARCHAR(150),  IN `IDZona` INT(11))
 BEGIN
-    SET
-    `p_Nome` := CONCAT("'", `p_Nome`, "'"),
-    `p_Email` := CONCAT("'", `p_Email`, "'"),
-    `p_Pass` := CONCAT("'", `p_Pass`, "'"),
-    `p_Role` := CONCAT("'", `p_Role`, "'");
-
-
-    SET @`sql` := CONCAT('CREATE USER IF NOT EXISTS ', `p_Email`, ' IDENTIFIED BY ', `p_Pass`);
-	PREPARE `stmt` FROM @`sql`;
+    SET @`sql` = CONCAT('SELECT COUNT(IDZona) INTO @zona FROM zona WHERE IDZona=', `IDZona`);
+    PREPARE `stmt` FROM @`sql`;
     EXECUTE `stmt`;
 
+    IF @zona=0 THEN
+        SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = "Zona não existe.";
+    END IF;
 
-    SET @`sql` := CONCAT('GRANT ', `p_Role`, ' TO ', `p_Email`);
+    SET @uuid = uuid();
+    SET FOREIGN_KEY_CHECKS=0;
+    INSERT INTO cultura(IDCultura, IDUtilizador, IDZona, NomeCultura, Estado)
+    VALUES (@uuid, "NÃO_ATRIBUÍDA", `IDZona`, `nome_cultura`, 0);
+    SET FOREIGN_KEY_CHECKS=1;
+
+
+    SET @`sql` = CONCAT('SELECT s.LimiteSuperior INTO @TMax FROM sensor s WHERE s.TipoSensor = ''T'' AND s.IDZona=', `IDZona`);
+    PREPARE `stmt` FROM @`sql`;
+    EXECUTE `stmt`;
+
+    SET @`sql` = CONCAT('SELECT s.LimiteSuperior INTO @HMax FROM sensor s WHERE s.TipoSensor = ''H'' AND s.IDZona=', `IDZona`);
+    PREPARE `stmt` FROM @`sql`;
+    EXECUTE `stmt`;
+
+    SET @`sql` = CONCAT('SELECT s.LimiteSuperior INTO @LMax FROM sensor s WHERE s.TipoSensor = ''L'' AND s.IDZona=', `IDZona`);
+    PREPARE `stmt` FROM @`sql`;
+    EXECUTE `stmt`;
+
+    SET @`sql` = CONCAT('SELECT s.LimiteInferior INTO @TMin FROM sensor s WHERE s.TipoSensor = ''T'' AND s.IDZona=', `IDZona`);
+    PREPARE `stmt` FROM @`sql`;
+    EXECUTE `stmt`;
+
+    SET @`sql` = CONCAT('SELECT s.LimiteInferior INTO @HMin FROM sensor s WHERE s.TipoSensor = ''H'' AND s.IDZona=', `IDZona`);
+    PREPARE `stmt` FROM @`sql`;
+    EXECUTE `stmt`;
+
+    SET @`sql` = CONCAT('SELECT s.LimiteInferior INTO @LMin FROM sensor s WHERE s.TipoSensor = ''L'' AND s.IDZona=', `IDZona`);
     PREPARE `stmt` FROM @`sql`;
     EXECUTE `stmt`;
 
 
-    SET @`sql` := CONCAT('SET DEFAULT ROLE ', `p_Role`, ' FOR ', `p_Email`);
-    PREPARE `stmt` FROM @`sql`;
-    EXECUTE `stmt`;
-
-
-    SET
-    `p_Nome` := REPLACE(`p_Nome`,'''',''),
-    `p_Email` := REPLACE(`p_Email`,'''',''),
-    `p_Role` := REPLACE(`p_Role`,'''',''),
-    `p_Role` := SUBSTRING(`p_Role`, 1, 1);
-
-
-    INSERT INTO utilizador(IDUtilizador, NomeUtilizador, EmailUtilizador, TipoUtilizador)
-    VALUES (uuid(), `p_Nome`, `p_Email`, `p_Role`);
+    INSERT INTO parametrocultura(IDCultura, TipoSensor, ValorMax, ValorMin, ToleranciaMax, ToleranciaMin)
+    VALUES (@uuid, "T", @TMax, @TMin, 0, 0),
+           (@uuid, "H", @HMax, @HMin, 0, 0),
+           (@uuid, "L", @LMax, @LMin, 0, 0);
 
 
     DEALLOCATE PREPARE `stmt`;
