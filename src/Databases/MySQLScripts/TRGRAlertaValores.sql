5f6c7ba443648c770a1f72849e9d7167d1c17f2b
moonzn <raul.0515@hotmail.com>
2022-05-04 01:30:04 +0100

    triggers finais

--- src/Databases/MySQLScripts/TRGRAlertaValores.sql
+++ src/Databases/MySQLScripts/TRGRAlertaValores.sql
@@ -1,15 +1,15 @@
 DELIMITER $$
 
-DROP TRIGGER IF EXISTS `AlertaTolerancias` $$
+DROP TRIGGER IF EXISTS `AlertaValores` $$
 CREATE DEFINER=`root`@`localhost`
-TRIGGER `AlertaTolerancias` BEFORE INSERT ON `medicao` FOR EACH ROW
+TRIGGER `AlertaValores` BEFORE INSERT ON `medicao` FOR EACH ROW
 BEGIN
     DECLARE done INT DEFAULT FALSE;
     DECLARE id_cultura VARCHAR(50);
     DECLARE estado_ INT(11);
     DECLARE utilizador VARCHAR(50);
-    DECLARE TolMin DECIMAL(5,2);
-    DECLARE TolMax DECIMAL(5,2);
+    DECLARE ValMin DECIMAL(5,2);
+    DECLARE ValMax DECIMAL(5,2);
     DECLARE nome_cultura VARCHAR(50);
     DECLARE cur1 CURSOR FOR SELECT IDCultura, Estado, IDUtilizador FROM cultura WHERE IDZona = NEW.IDZona;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
@@ -25,15 +25,15 @@ BEGIN
 
         IF (estado_ = 1 AND NOT STRCMP("NÃO_ATRIBUÍDA", utilizador) = 0) THEN
 
-            SELECT ToleranciaMax, ToleranciaMin INTO TolMax, TolMin FROM parametrocultura WHERE IDCultura = id_cultura AND TipoSensor = NEW.TipoSensor;
+            SELECT ValorMax, ValorMin INTO ValMax, ValMin FROM parametrocultura WHERE IDCultura = id_cultura AND TipoSensor = NEW.TipoSensor;
             SELECT NomeCultura INTO nome_cultura FROM cultura WHERE IDCultura = id_cultura;
 
-            IF (NEW.Valor <= TolMin) THEN
+            IF (NEW.Valor <= ValMin) THEN
                 INSERT INTO alerta(IDAlerta, IDZona, NomeCultura, IDCultura, IDUtilizador, IDSensor, TipoSensor, TipoAlerta, Datetime, Valor, Mensagem)
-                VALUES (uuid(), NEW.IDZona, nome_cultura, id_cultura, utilizador, NEW.IDSensor, NEW.TipoSensor, 'T', CURRENT_TIMESTAMP, NEW.Valor, "Medição excedeu tolerância mínima.");
-            ELSEIF (NEW.Valor >= TolMax) THEN
+                VALUES (uuid(), NEW.IDZona, nome_cultura, id_cultura, utilizador, NEW.IDSensor, NEW.TipoSensor, 'V', CURRENT_TIMESTAMP, NEW.Valor, "Medição excedeu valor mínimo do sensor.");
+            ELSEIF (NEW.Valor >= ValMax) THEN
                 INSERT INTO alerta(IDAlerta, IDZona, NomeCultura, IDCultura, IDUtilizador, IDSensor, TipoSensor, TipoAlerta, Datetime, Valor, Mensagem)
-                VALUES (uuid(), NEW.IDZona, nome_cultura, id_cultura, utilizador, NEW.IDSensor, NEW.TipoSensor, 'T', CURRENT_TIMESTAMP, NEW.Valor, "Medição excedeu tolerância máxima.");
+                VALUES (uuid(), NEW.IDZona, nome_cultura, id_cultura, utilizador, NEW.IDSensor, NEW.TipoSensor, 'V', CURRENT_TIMESTAMP, NEW.Valor, "Medição excedeu valor máximo do sensor.");
             END IF;
         END IF;
     END LOOP alert_loop;
